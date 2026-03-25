4de07d33b70341737697b04e497e547d047e4921
suyogvibhuti <suyog.vibhuti@gmail.com>
2026-03-25 01:17:08 -0700

    extended to 10 seconds, little debug thing too

--- dgfemfull.cpp
+++ dgfemfull.cpp
@@ -82,10 +82,11 @@ void dgfem() {
 	aprime[1][0] = c * (3 * a[0][0] - a[1][0] - 2 * a[1][K - 1]);
 
 	// To-Do: Develop ODE integrator (forward euler or RK4), apply to aprime values until desired time t, make q list: q_i = a_0i(1 - x) + a_1i(x). Output q list?
-    double time = 1;
+    double time = 10;
     double tStep = 0.1;
     for (int secondsCount = 0; secondsCount < time; secondsCount++) {
         for (int count = 0; count < (1.0 / tStep); count++) {
+			cout << a[0][6] << "\n";
             forwardEuler(a, aprime, tStep);
             for (int i = 1; i < K; i++) {
 		        // Formulas derived from page in notebook, lots of matrix multiplication
